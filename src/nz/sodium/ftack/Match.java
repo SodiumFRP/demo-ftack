29281b333605d5cafa2a53e316833706bfd84f26
Stephen Blackheath <docks.cattlemen.stephen@blacksapphire.com>
2016-07-27 06:30:52 +0200

    A block whose position is a function of time.

--- src/nz/sodium/ftack/Match.java
+++ src/nz/sodium/ftack/Match.java
@@ -26,42 +26,18 @@ public class Match {
 
 	public Match(TimerSystem<Double> sys, Stream<Unit> sClick)
 	{
-		Cell<Boolean> direction = sClick.accum(false, (u, d) -> !d);
-	    CellLoop<Block> block = new CellLoop<>();
-		double t0 = sys.time.sample();
-        Cell<Double> tLevel = sClick.snapshot(sys.time).hold(t0);
-		Cell<Float> displacement = sys.time.lift(tLevel,
-				(t, tLev) -> 10 - (float)(t - tLev) * 12);
-		CellLoop<Array<Block>> stack = new CellLoop<>();
-        Stream<Optional<Array<Block>>> sUpdate =
-        		sClick.snapshot(stack, block, displacement, direction,
-        				(u, s, blk, disp, dir) -> {
-        			int level = s.length();
-        			Block below = s.get(level-1);
-        			Optional<Block> oBlk = blk.overlap(below, disp, dir);
-        			if (oBlk.isPresent())
-	        		    return Optional.of(s.append(oBlk.get()));
-        			else
-        				return Optional.empty();
-        		});
-        stack.loop(Stream.filterOptional(sUpdate).hold(initialStack));
-        block.loop(displacement.lift(stack, direction,
-        		(disp, s, dir) -> {
-        			int level = s.length();
-        			Block blk = s.get(level-1).setColour(levelColour(level));
-        			return dir ? blk.translate(0, disp, 1)
-        					   : blk.translate(disp, 0, 1);
-        		}
-        ));
-        scene = stack.lift(block, sys.time, tLevel,
-        		(blks, blk, t, tLev) -> {
-        			float tt = (float)(t - tLev) * 3;
-        			if (tt > 1) tt = 1;
-        		    return new Scene(blks.append(blk), (float)blks.length() + tt, 1);
-        		});
-        sGameOver = Stream.filterOptional(sUpdate.map(su ->
-            su.isPresent() ? Optional.empty()
-            		       : Optional.of(Unit.UNIT)));
+		Cell<Double> time = sys.time;
+		double t0 = time.sample();
+        scene = time.map(t ->
+	        		new Scene(
+        				Array.of(
+        						initialBlock.translate(
+        								(float)(t - t0), 0, 0)
+						),
+        				0, 1
+					)
+	    		);
+        sGameOver = sClick;
 	}
 
 	public final Cell<Scene> scene;
